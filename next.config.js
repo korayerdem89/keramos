21bff8f4ac4e45e563b96ad5df313f38f0ce4cc3
korayerdem89 <korayerdem89@gmail.com>
2024-07-01 00:20:43 +0300

    .env.production created

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
-const createNextIntlPlugin = require('next-intl/plugin');
- 
+const createNextIntlPlugin = require("next-intl/plugin");
+
 const withNextIntl = createNextIntlPlugin();
- 
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
- 
+const nextConfig = {
+  env: {
+    API_KEY: process.env.API_KEY,
+  },
+};
+
 module.exports = withNextIntl(nextConfig);
