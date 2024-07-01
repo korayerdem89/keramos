b371bbc0254a9d963128b660f0a70a9ddd250936
korayerdem89 <korayerdem89@gmail.com>
2024-07-01 14:10:47 +0300

    speedinsight by vercel added

--- src/components/hero/hero-9/HeroSection.jsx
+++ src/components/hero/hero-9/HeroSection.jsx
@@ -24,6 +24,7 @@ const HeroSection = () => {
                 <Player
                   fluid={true}
                   controls={false}
+                  playsInline
                   autoPlay
                   muted
                   loop
