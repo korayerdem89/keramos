7123c6ec1580cee3874b285d4669a95e20d99cca
korayerdem89 <korayerdem89@gmail.com>
2024-07-01 15:42:35 +0300

    video fixed

--- src/components/hero/hero-9/HeroSection.jsx
+++ src/components/hero/hero-9/HeroSection.jsx
@@ -6,7 +6,7 @@ import React from "react";
 const HeroSection = () => {
   const t = useTranslations("Banner");
   return (
-    <div className="hero-section">
+    <div className="hero-section bg-white sm:bg-dark">
       <div className="video-background">
         <ReactPlayer
           url="https://vimeo.com/973018476?share=copy"
@@ -24,7 +24,7 @@ const HeroSection = () => {
         />
       </div>
       <div className="content-overlay">
-        <h1 className="sm:text-16 md:text-20 xl:text-40  sm:w-1/1 lg:w/1-2 fw-300">
+        <h1 className="sm:d-none md:text-20 xl:text-40  sm:w-1/1 lg:w/1-2 fw-300">
           {t("descriptions")}
         </h1>
       </div>
